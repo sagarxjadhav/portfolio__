b2ad7c73defafd025aeea41f8417601e1f34c52d
Sagar Jadhav <93977940+sagarxjadhav@users.noreply.github.com>
2024-09-20 00:46:25 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio__/', // Make sure this matches your GitHub repo name
+  base: '/', // Make sure this matches your GitHub repo name
 });
